63d731b5aa17c9202dac4fc8ed6deb261d8465fb
Miguel Ramos <miguelrb@microsoft.com>
2022-06-21 10:35:38 -0700

    Add project files.

---
+++ CameraCaptureUI/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+
+using Windows.Foundation;
+using Windows.Media.Capture;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.Graphics.Imaging;
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Media.Imaging;
+
+using System.Numerics;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Windows.Graphics;
+using Windows.Graphics.Capture;
+using Windows.Graphics.DirectX;
+using Windows.Graphics.DirectX.Direct3D11;
+using Windows.System;
+using Windows.UI;
+using Windows.UI.Composition;
+
+
+namespace CameraCaptureUI
+{
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            var captureUI = new Windows.Media.Capture.CameraCaptureUI();
+
+            // CameraCaptureUI doesn't implement Win32 interop pattern so it's not possivle to use it in WinUI3.
+            // as alternative, use the Windows.Media.Capture.MediaCapture APIs 
+            
+            //var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            //WinRT.Interop.InitializeWithWindow.Initialize(captureUI, hWnd);
+
+            captureUI.PhotoSettings.Format = CameraCaptureUIPhotoFormat.Jpeg;
+            captureUI.PhotoSettings.CroppedSizeInPixels = new Size(200, 200);
+
+            StorageFile photo = await captureUI.CaptureFileAsync(CameraCaptureUIMode.Photo);
+
+            if (photo == null)
+            {
+                // User cancelled photo capture
+                return;
+            }
+        }
+    }
+}
+
---
+++ MediaCapture/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿
+using System;
+using System.Threading.Tasks;
+
+using Windows.Media.MediaProperties;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Media.Imaging;
+
+namespace MediaCapture
+{
+
+    public sealed partial class MainWindow : Window
+    {
+        Windows.Media.Capture.MediaCapture _mediaCapture;
+
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+        async private void CapturePhoto_Click(object sender, RoutedEventArgs e)
+        {
+            if (_mediaCapture == null)
+            {
+                _mediaCapture = new Windows.Media.Capture.MediaCapture();
+                await _mediaCapture.InitializeAsync();
+            }
+
+            await TakePhoto();
+
+            //await SavePhoto(imgFormat);
+        }
+
+        private async Task TakePhoto()
+        {
+            var encodingProperties = ImageEncodingProperties.CreateJpeg();
+            var bitmap = new WriteableBitmap((int)imageViewer.Width, (int)imageViewer.Height);
+            using (var imageStream = new InMemoryRandomAccessStream())
+            {
+                // Take photo
+                await _mediaCapture.CapturePhotoToStreamAsync(encodingProperties, imageStream);
+                await imageStream.FlushAsync();
+                imageStream.Seek(0);
+                bitmap.SetSource(imageStream);
+                imageViewer.Source = bitmap;
+            }
+        }
+
+        private async Task TakePhotoAndSave()
+        {
+            var encodingProperties = ImageEncodingProperties.CreateJpeg();
+
+            StorageFile file = await ApplicationData.Current.LocalFolder.CreateFileAsync(
+                "TestPhoto.jpg",
+                CreationCollisionOption.GenerateUniqueName);
+
+            // Take photo
+            await _mediaCapture.CapturePhotoToStorageFileAsync(encodingProperties, file);
+
+            BitmapImage bmpImage = new BitmapImage(new Uri(file.Path));
+
+            // imagePreview is a <Image> object defined in XAML
+            imageViewer.Source = bmpImage;
+        }
+    }
+}
